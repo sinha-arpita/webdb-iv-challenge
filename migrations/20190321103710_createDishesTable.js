923d85303e540694b388621c6fc4002e7986217a
Arpita Sinha <sinha.arpita@gmail.com>
2019-03-23 12:19:09 -0700

    minor issue fixed

--- migrations/20190321103710_createDishesTable.js
+++ migrations/20190321103710_createDishesTable.js
@@ -4,9 +4,7 @@ exports.up = function(knex, Promise) {
       tbl.increments();
       tbl.string('dish_name').notNullable()
     })
-   };exports.up = function(knex, Promise) {
-    return knex.schema.createTable('dishes', tbl => {
-      tbl.increments();
-      tbl.string('dish_name').notNullable()
-    })
+   };
+   exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('dishes')
    };
